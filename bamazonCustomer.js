b8bff77516c5a4755cf298889cf2ac385c4ce977
KSentak <k.sentak@outlook.com>
2019-11-15 12:53:53 -0500

    Created a purchase function that takes in an id and quantity

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,5 +1,6 @@
 require("dotenv").config();
 var mysql = require("mysql");
+var inquirer = require("inquirer");
 const cTable = require('console.table');
 
 
@@ -11,16 +12,46 @@ var connection = mysql.createConnection({
   database: process.env.DATABASE
 });
 
-connection.connect(function(err) {
+connection.connect(function (err) {
+  if (err) throw err;
+  begin();
+});
+
+function begin() {
+  connection.query("SELECT * FROM products", function (err, res) {
     if (err) throw err;
-    afterConnection();
+    console.table(res);
+    purchase();
+    connection.end();
   });
+}
 
-  function afterConnection() {
-    connection.query("SELECT * FROM products", function(err, res) {
-      if (err) throw err;
+function purchase() {
+  inquirer
+    .prompt([
+      {
+        name: "item",
+        type: "input",
+        message: "What is the id of the item you would like to purchase?"
+      },
 
-      console.table(res);
-      connection.end();
+      {
+        name: "quantity",
+        type: "input",
+        message: "How many of this item would you like to purchase? (Enter quantity)"
+      }
+      
+    ])
+    .then(function (answer) {
+      console.log("Your order has been placed")
+      
+      console.log(answer.item);
+      console.log(answer.quantity);
+      
+
+      //Once order has been placed, check database to see IF answer.quantity < database quantity
+      //IF answer.quantity > database quantity console.log("Insufficent quantity! There was an error placing your order. Please try again.")
+      //IF answer.quantity < database quantity allow purchase to go through and update database
+      //Display updated table
     });
 }
